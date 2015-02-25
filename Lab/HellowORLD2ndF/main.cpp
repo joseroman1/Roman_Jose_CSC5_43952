0898ccd9a1d99ea2ffaa7a199196738aecc10f4e
joseroman1 <joseroman@outlook.com>
2015-02-25 10:44:51 -0800

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Lab/HellowORLD2ndF/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *Created on February 25, 2015, 10:42 AM
+ *         Purpose: Wasn't satisfied with 1 F
+ *                  I wanted another
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
