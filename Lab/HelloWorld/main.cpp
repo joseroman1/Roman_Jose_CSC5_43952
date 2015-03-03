dacb437ede3a2c172eeee02a68a4b8519a04f723
joseroman1 <joseroman@outlook.com>
2015-03-02 19:31:10 -0800

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Roman
+ *Created on March 1, 2015, 12:45 PM
+ * Purpose: Set Up
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants 
+
+
+//Function Prototypes
+ 
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World 
+    cout<<"Hello World"<<endl;
+    //Exit stage Right!
+    return 0;
+}
+
+
