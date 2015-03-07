19b297479232bfa28e334140007ad9b225b4b778
joseroman1 <joseroman@outlook.com>
2015-03-06 21:37:06 -0800

    Signed-off-by: joseroman1 <joseroman@outlook.com>

--- Hmwk/Assignment_1/Savitch_8thEd_Chap1_ProgProj_Prob10/main.cpp
+++ Hmwk/Assignment_1/Savitch_8thEd_Chap1_ProgProj_Prob10/main.cpp
@@ -4,35 +4,35 @@
  *Created on March 4, 2015, 9:40 AM
  *       Purpose: Homework Assignment
  */
-
-//System Libraries
+    
+ //System Libraries
 #include <iostream>
 using namespace std;
 
 //User Libraries
 
-//Global Constants 
+//Global Constants
 
 //Function Prototypes
 
 //Execution Begins Here!
-
 int main(int argc, char** argv) {
-    //Output of Large CS letters.
-    
-    cout<<"        x x x        \n";
-    cout<<"      x      x      \n";
-    cout<<"     x             \n";
-    cout<<"    x               \n";
-    cout<<"    x                \n";
-    cout<<"    x                 n";
-    cout<<"     x                 \n";
-    cout<<"      x       x       \n";
-    cout<<"        x x x        \n";
-    
-  
-
-    //Exit right stage!
-
+    //Declare variables
+    char c;//Character to be used to display C
+    //Prompt for the letter C to be user
+    cout<<"What letter would you like to use for Big C"<<endl;
+    cin>>c;
+    //Output Big C
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"  "<<c<<"    "<<c<<endl;
+    cout<<" "<<c<<"      "<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<" "<<c<<"      "<<c<<endl;
+    cout<<"  "<<c<<"    "<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    //Exit stage right
     return 0;
-}
+}   
