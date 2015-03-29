3bcd942f583733426ba33c0613713f6c9ac602da
joseroman <joseroman@outlook.com>
2015-03-29 15:49:20 -0700

    Signed-off-by: joseroman <joseroman@outlook.com>

--- Hmwk/Assignment_3/Savitch_8thEd_Chap2_ProgProj_Prob1/main.cpp
+++ Hmwk/Assignment_3/Savitch_8thEd_Chap2_ProgProj_Prob1/main.cpp
@@ -1,8 +1,7 @@
-/* 
- * File:   main.cpp
+/* File:   main.cpp
  * Author: Jose Roman
  * Created on March 23, 2015, 10:19 AM
- *      Purpose: Paper-Rock-Scissor
+ * Purpose: Paper-Rock-Scissor
  */
 
 //System Libraries 
@@ -21,17 +20,32 @@ using namespace std;
 //Execution begins here!
 int main(int argc, char** argv) {
   //Declare Variables
-    char paper,rock,scissor, input1,input2;
+    char user1, user2, r, s, p;
     
-    // Explanation to the rules
-    cout<<"This program will score a Rock-Paper-Scissor game between";
-    cout<<" two users. "<<endl;
-    cout<<"Each user will input R for rock, P for paper, and S for scissor";
-    cout<<" when prompt to."<<endl;
-    //Output Prompt
-    cout<<"First player input R for rock, P for paper or S for scissor"<<endl;
-    cin>>input1;
-
+    cout<<"This program allows you to play a game of rock, "<<endl;
+    cout<<"paper, scissors with two players."<<endl;
+    cout<<"Input s=scissors r=rock and p=paper"<<endl;
+    cout<<"Player 1, enter your choice."<<endl;
+    cin>>user1;
+    cout<<"Player 2, enter your choice."<<endl;
+    cin>>user2;
+    
+    
+    if (user1=='r'&&user2=='s')
+        {cout<<"Player 1 wins! Rock crushes scissors!"<<endl;}
+    else if (user1=='s'&&user2=='r')
+        {cout<<"Player 2 wins! Rock crushes scissors!"<<endl;}
+    else if ((user1=='p')&&(user2=='s'))
+        {cout<<"Player 2 wins! Scissors cuts paper!"<<endl;}
+    else if ((user1=='s')&&(user2=='p'))
+        {cout<<"Player 1 wins! Scissors cuts paper!"<<endl;}
+    else if ((user1=='p')&&(user2=='r'))
+        {cout<<"Player 1 wins! Paper covers rock!"<<endl;}
+    else if ((user1=='r')&&(user2=='p'))
+        {cout<<"Player 2 wins! Paper covers rock!"<<endl;}
+    else 
+        {cout<<"It's a draw!"<<endl;}
+    
+    
     return 0;
 }
-
