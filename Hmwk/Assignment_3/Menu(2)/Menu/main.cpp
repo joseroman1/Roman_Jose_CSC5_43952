238e5b10167fbbef90478dd2609abecdd0f819ae
joseroman <joseroman@outlook.com>
2015-03-29 17:47:39 -0700

    Signed-off-by: joseroman <joseroman@outlook.com>

--- Hmwk/Assignment_3/Menu(2)/Menu/main.cpp
+++ Hmwk/Assignment_3/Menu(2)/Menu/main.cpp
@@ -4,7 +4,7 @@
  * Purpose:  Assignment 3 Homework Menu
  * Problem 1 -> Rock-Paper-Scissor Game
  * Problem 2 -> Cable Bill
- * Problem 3 -> Divide the numbers between 1 and 10
+ * Problem 3 -> Long Distance Call Calculator
  * Modified on Mar 23rd, 2015
  */
 
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     //Display the selection
     cout<<"Type 1 to solve Savitch Chapter 3 Problem 1"<<endl;
     cout<<"Type 2 to solve Gaddis Chapter 4 Problem 22"<<endl;
-    cout<<"Type 3 to solve problem 3"<<endl;
+    cout<<"Type 3 to solve Savitch 8th Chapter 3 Problem 5"<<endl;
     cout<<"Type 4 to solve problem 4"<<endl;
     cout<<"Type 5 to solve problem 5"<<endl;
     cout<<"Type anything else to quit with no solutions."<<endl;
@@ -38,19 +38,33 @@ int main(int argc, char** argv) {
     //Solve a problem that has been chosen.
     switch(choice){
             case '1':{
-                //Declare Variables
-    char paper,rock,scissor, input1,input2;
+                
+    //Declare Variables
+    char user1, user2, r, s, p;
     
-    // Explanation to the rules
-    cout<<"This program will score a Rock-Paper-Scissor game between";
-    cout<<" two users. "<<endl;
-    cout<<"Each user will input R for rock, P for paper, and S for scissor.";
-    cout<<" when prompt to."<<endl;
-    //Output Prompt
-    cout<<"First player input R for rock, P for paper or S for scissor"<<endl;
-    cin>>input1;
-
+    cout<<"This program allows you to play a game of rock, "<<endl;
+    cout<<"paper, scissors with two players."<<endl;
+    cout<<"Input s=scissors r=rock and p=paper"<<endl;
+    cout<<"Player 1, enter your choice."<<endl;
+    cin>>user1;
+    cout<<"Player 2, enter your choice."<<endl;
+    cin>>user2;
     
+    
+    if (user1=='r'&&user2=='s')
+        {cout<<"Player 1 wins! Rock crushes scissors!"<<endl;}
+    else if (user1=='s'&&user2=='r')
+        {cout<<"Player 2 wins! Rock crushes scissors!"<<endl;}
+    else if ((user1=='p')&&(user2=='s'))
+        {cout<<"Player 2 wins! Scissors cuts paper!"<<endl;}
+    else if ((user1=='s')&&(user2=='p'))
+        {cout<<"Player 1 wins! Scissors cuts paper!"<<endl;}
+    else if ((user1=='p')&&(user2=='r'))
+        {cout<<"Player 1 wins! Paper covers rock!"<<endl;}
+    else if ((user1=='r')&&(user2=='p'))
+        {cout<<"Player 2 wins! Paper covers rock!"<<endl;}
+    else 
+        {cout<<"It's a draw!"<<endl;}
                 break;
             }
             case '2':{
@@ -109,15 +123,75 @@ int main(int argc, char** argv) {
                 break;
             }
             case '3':{
-                float quot=1.0f/2/3/4/5/6/7/8/9/10;
-                cout<<endl;
-                cout<<"Solution to 3 quot(1->10)="<<quot<<endl;
-                cout<<endl;
+                cout<<"This program wil calculate the cost of your long-distance phone call.\n";
+    char choose;
+    cout<<"Would you like to proceed?. Type Y for yes or N for no.\n";
+    cin>>choose;
+    while(choose=='Y'||choose=='y'){
+    char day; //Variabll for the day
+    int time; //Variable for the time of the day
+    int duration;// Duration of call in minutes
+    
+    cout<<"Input the day of the call in the following format :\n";
+    cout<<"For Monday type 'M'"<<endl;
+    cout<<"For Tuesday type 'T'"<<endl;
+    cout<<"For Wednesday type 'W' "<<endl;
+    cout<<"For Thursday type 'TH'"<<endl;
+    cout<<"For Friday type 'F' "<<endl;
+    cout<<"For Saturday type S"<<endl;
+    cout<<"For Sunday type 'A'"<<endl;
+    cout<<"This program is case-sensitive, so please use the specified symbols\n";
+    cin>>day;
+   
+    //Time
+    cout<<"Input the time of the call in 24 hour notation/military time. "<<endl;
+    cout<<"Example. 1330 or 745. etc.  "<<endl;
+    cin>>time;
+    
+    cout<<"What is the duration of the call in minutes?"<<endl;
+    cin>>duration;
+    
+ 
+    switch(day) {
+    case 'M':
+    case 'T':
+    case 'W':
+    case 'P':
+    case 'F':
+            if(time>=800&&time<=1800){
+                float rate = .40*duration; //Calulates the cost of the phone call
+                cout<<"the phone call cost $"<<rate<<endl;
+                
+            }
+            else{
+                float rate =.25*duration;
+                cout<<"the phone call cost $"<<rate<<endl;
+            }
+        break;
+    case 'S':
+    case 'A':
+            float rate =duration*.15;
+            cout<<"the phone call cost $"<<rate<<endl;
+            break;
+            
+      
+      
+            
+    
+    }
+    cout<<"Would you like to repeat this program? Y/N.\n";
+        cin>>choose;
+    }
                 break;
             }
+        case '4':{
+            
+            break;
+        }
+        
             default:{
                     cout<<"Exit?"<<endl;
             }
     };
     return 0;
-}
+}  
---
+++ Hmwk/Assignment_3/Savitch_8thEd_Chapter3_ProjProg_Prob5/main.cpp
@@ -0,0 +1,82 @@
+/* File:   main.cpp
+ * Author: Jose Roman
+ * Created on March 25, 2015, 10:15 PM
+ * Purpose: Paper-Rock-Scissor
+ */
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+
+//Global Constants
+
+
+//Function Prototypes
+
+
+//Execution begins here!
+int main(int argc, char** argv) {
+  cout<<"This program wil calculate the cost of your long-distance phone call.\n";
+    char choose;
+    cout<<"Would you like to proceed?. Type Y for yes or N for no.\n";
+    cin>>choose;
+    while(choose=='Y'||choose=='y'){
+    char day; //Variabll for the day
+    int time; //Variable for the time of the day
+    int duration;// Duration of call in minutes
+    
+    cout<<"Input the day of the call in the following format :\n";
+    cout<<"For Monday type 'M'"<<endl;
+    cout<<"For Tuesday type 'T'"<<endl;
+    cout<<"For Wednesday type 'W' "<<endl;
+    cout<<"For Thursday type 'TH'"<<endl;
+    cout<<"For Friday type 'F' "<<endl;
+    cout<<"For Saturday type S"<<endl;
+    cout<<"For Sunday type 'A'"<<endl;
+    cout<<"This program is case-sensitive, so please use the specified symbols\n";
+    cin>>day;
+   
+    //Time
+    cout<<"Input the time of the call in 24 hour notation/military time. "<<endl;
+    cout<<"Example. 1330 or 745. etc.  "<<endl;
+    cin>>time;
+    
+    cout<<"What is the duration of the call in minutes?"<<endl;
+    cin>>duration;
+    
+ 
+    switch(day) {
+    case 'M':
+    case 'T':
+    case 'W':
+    case 'P':
+    case 'F':
+            if(time>=800&&time<=1800){
+                float rate = .40*duration; //Calulates the cost of the phone call
+                cout<<"the phone call cost $"<<rate<<endl;
+                
+            }
+            else{
+                float rate =.25*duration;
+                cout<<"the phone call cost $"<<rate<<endl;
+            }
+        break;
+    case 'S':
+    case 'A':
+            float rate =duration*.15;
+            cout<<"the phone call cost $"<<rate<<endl;
+            break;
+            
+      
+      
+            
+    
+    }
+    cout<<"Would you like to repeat this program? Y/N.\n";
+        cin>>choose;
+    }
+    
+    return 0;
+}
