2472615c4b0b6107b17faf14c7075fa351bb3d3c
joseroman1 <joseroman@outlook.com>
2015-05-11 21:14:24 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

--- Projects/Project_1/main.cpp
+++ Projects/Project_1/main.cpp
@@ -2,6 +2,29 @@
  * File:   main.cpp
  * Author: Jose Roman
  * Created on May 4, 2015, 10:33 AM
- *      Purpose: Project 1: Password Game
+ *      Purpose: Project 1: Hangman Game
  */
 
+//System Libraries
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+const char  *WORD;
+enum FNDTYPE{NFOUND, FOUND, ARDFOUND};// Compare Results
+
+//Function Prototypes
+void rules();//rules of the games
+void rGame(char fBlank[],int,char,int,int);
+//Execution Begins Here!
+
+int main (int argc, char** argv){
+    
+    return 0;
+}
