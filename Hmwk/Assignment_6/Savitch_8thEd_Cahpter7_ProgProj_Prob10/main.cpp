9a67027aa1a451b0707ca894ea22404fe4b19682
joseroman1 <joseroman@outlook.com>
2015-05-20 10:38:23 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

--- Hmwk/Assignment_6/Savitch_8thEd_Cahpter7_ProgProj_Prob10/main.cpp
+++ Hmwk/Assignment_6/Savitch_8thEd_Cahpter7_ProgProj_Prob10/main.cpp
@@ -17,34 +17,33 @@ void sBoard(char []);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
-    char SIZE=9;
-    int board[SIZE];
-    int i;
+    const char SIZE=9;
+    char board[SIZE];
     int nMoves=0;
     char wTurn;
     int move;
-    for (i=0;i<9;i++){
-        board[i]= 'i' + i;
+    for (int i=0;i<9;i++){
+        board[i]= '1' + i;
     }
     while (nMoves < 9){
         sBoard(board);
         cout<<"Enter move: "<<endl;
         cin>>move;
-        if((move<1) || (move>9))
-            cout<<"Invalid move, try again"<<endll
+        if((move < 1) || (move > 9))
+            cout<<"Invalid move, try again"<<endl;
         else{
             move--;
-            if ((board[move]=="X") || (board[move]=='O'))
+            if ((board[move]=='X') || (board[move]=='O'))
                 cout<<"That space is taken. "<<endl;
             else{
-                board[move]=wTurn;
+                board[move]= wTurn;
                 //Switch turn
                 if(wTurn == 'X')
-                    wTurn= 'O';
-                else{
+                    wTurn = 'O';
+                else
                     wTurn = 'X';
                     nMoves++;
-                }
+                
             }
         }
     }
@@ -53,4 +52,13 @@ int main(int argc, char** argv) {
         cout<<endl<<"Game over.."<<endl;
         
     return 0;
+}
+void sBoard(char board[]){
+    cout<<endl;
+    for (int i=0;i<9;i++){
+        cout<<board[i]<<" ";
+        if (((i+1) % 3) == 0)cout<<endl;
+        
+    }
+    cout<<endl;
 }
