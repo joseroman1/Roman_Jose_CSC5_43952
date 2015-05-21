04ad648feed590176d862061869a716999ae0ff9
joseroman1 <joseroman@outlook.com>
2015-05-21 13:48:00 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Hmwk/Assignment_6/Savitch_9thEd_Chapter7_ProgProj_Prob1/main.cpp
@@ -0,0 +1,69 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Roman
+ * Created on May 21, 2015, 12:43 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+bool fLast(int [],int);
+void printArray(int [],int);
+
+int main(int argc, char** argv) {
+    int SIZE=5;
+    int array[SIZE];
+    
+    cout<<"Give me "<<SIZE<<" numbers. Press return after each number"<<endl;
+    cin>>array[0]>>array[1]>>array[2]>>array[3]>>array[4];
+    
+    //Print Array
+    printArray(array, SIZE);
+    //Find digit 2
+    fLast(array, SIZE);
+
+    return 0;
+}
+void printArray(int a[], int SIZE){
+    for(int i=0;i<SIZE;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+bool fLast(int a[],int SIZE){
+    bool sWith = false;//starts with
+    bool eWith = false;//Ends with
+    bool hTwo = false;// has two
+    
+    for(int i=0;i<SIZE;i++){
+        if (i == 0){//check if the array starts with two
+            if(a[i] == 2)
+                sWith =true;
+        }
+        else if (i == (SIZE-1)){//
+            if(a[i] == 2)
+                eWith = true;
+        }
+        else continue;
+    }
+    if (sWith && eWith){
+        cout<<" Array both starts and ends with a digit of 2."<<endl;
+        hTwo = true;
+    }
+    else if (sWith){
+        cout<<"Array starts with a digit of 2."<<endl;
+       hTwo = true;
+    }
+    else if (eWith){
+        cout<<"Array ends with a digit of 2."<<endl;
+        hTwo = true;
+    }
+    else 
+        cout<<"Array does not start or end in a digit of 2";
+    return hTwo;
+}
--- Hmwk/Assignment_6/Savitch_9thEd_Chapter7_ProgProj_Prob1/nbproject/Package-Debug.bash
+++ Hmwk/Assignment_6/Savitch_9thEd_Chapter7_ProgProj_Prob1/nbproject/Package-Debug.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_2
-OUTPUT_BASENAME=cppapplication_2
-PACKAGE_TOP_DIR=cppapplication2/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_9thed_chapter7_progproj_prob1
+OUTPUT_BASENAME=savitch_9thed_chapter7_progproj_prob1
+PACKAGE_TOP_DIR=savitch9thedchapter7progprojprob1/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/cppapplication2/bin"
+makeDirectory "${NBTMPDIR}/savitch9thedchapter7progprojprob1/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cppapplication2.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/savitch9thedchapter7progprojprob1.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cppapplication2.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/savitch9thedchapter7progprojprob1.tar *
 checkReturnCode
 
 # Cleanup
