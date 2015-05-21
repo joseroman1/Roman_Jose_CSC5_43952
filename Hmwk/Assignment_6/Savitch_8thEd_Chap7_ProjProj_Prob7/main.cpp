b28f5770800effe88c47e00ab568d005e1e7f542
joseroman1 <joseroman@outlook.com>
2015-05-20 19:02:04 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Hmwk/Assignment_6/Savitch_8thEd_Chap7_ProjProj_Prob7/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: JR
+ * Created on May 20, 2015, 6:53 PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+void input(char [],int);//Prompt the user for number
+void chInt(const char [],int,int[]);//Change char array to int array
+void reverse(int [],const int []);//and two number
+void add(int [],const int [],int,const int [],int);
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    const int SIZE=25;
+    char ipchar1[SIZE];
+
+    return 0;
+}
+
---
+++ Hmwk/Assignment_6/Savitch_8thEd_Chapter7_ProgProj_Prob7/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: JR
+ *
+ * Created on May 20, 2015, 6:52 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
