22cd75c2769b62f3e3708dab6801dd6eb2f34dd7
joseroman1 <joseroman@outlook.com>
2015-05-21 21:37:05 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

--- Hmwk/Assignment_6/Savitch_9thEd_Chap7_ProgProj_Prob3/main.cpp
+++ Hmwk/Assignment_6/Savitch_9thEd_Chap7_ProgProj_Prob3/main.cpp
@@ -7,7 +7,7 @@
 //System Libraries
 #include <iostream>
 using namespace std;
-
+ 
 //User Libraries
 
 //Global Constants
@@ -20,9 +20,44 @@ void printArray(char [],int);
 int main(int argc, char** argv) {
     char SIZE=10;
     char array[SIZE];
-    
-    
+    array[0] = 'a';  
+    array[1] = 'b';
+    array[2] = 'a';
+    array[3] = 'c';
+    int sizes =4;
+    cout<<"Before removing: ";
+    printArray(array,sizes);
+    delete_repeats(array,sizes);
+    cout<<"After removing: ";
+    printArray(array,sizes);
 
     return 0;
 }
-
+void printArray(char a[],int sizes){
+    for(int i=0;i<sizes;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+void delete_repeats(char a[],int sizes){
+//Go through and find the repeated characters
+for(int i=0;i<sizes;i++){
+    for(int j=i+1;j<sizes;j++){
+        //.if the character is repeated
+        if(a[i] == a[j])
+            a[j]= 0;
+        else 
+            continue;
+    }
+}
+ //Go throught and find the deleted character
+for(int i = 0;i<sizes;i++){
+    //empty space move it forward
+    if(a[i] == 0){
+        a[i] = a[i+1];
+        a[i+1] = 0;
+    }
+    else
+        continue;
+}
+}
+    
