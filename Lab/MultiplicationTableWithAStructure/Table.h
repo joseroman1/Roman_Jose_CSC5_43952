759d74cf6a7861b66a7112a75975fa90b0352f0a
joseroman1 <joseroman@outlook.com>
2015-06-03 08:56:52 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Lab/MultiplicationTableWithAStructure/Table.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Table.h
+ * Author: rcc
+ *
+ * Created on June 3, 2015, 8:37 AM
+ */
+
+#ifndef TABLE_H
+#define	TABLE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TABLE_H */
+
---
+++ Lab/MultiplicationTableWithAStructure/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 3, 2015, 8:36 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
